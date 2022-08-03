780c82892675522454c16c16910863c63e767cea
Atharva-Sabde <atharvaa.sabde@gmail.com>
2022-08-03 10:41:55 +0530

    2aug

---
+++ cra/func-comp/src/SimpProdCat/SimpleProductCatalogue.js
@@ -0,0 +1,15 @@
+function SimpleProductCatalogue(){
+    return(
+        <>
+        <h1> CheapKart </h1>
+        <h3> Find the cheapest products at your doorsteps </h3>
+        
+
+        
+
+        
+        </>
+    )
+}
+
+export default SimpleProductCatalogue;
